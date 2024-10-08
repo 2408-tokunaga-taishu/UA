37a99f427d05dc43d58b47b4d6e123cb1dbc2566
tokunaga_taishu <tokunaga.taishu@alhinc.jp>
2024-10-08 14:49:45 +0900

    環境構築済み

--- src/main/java/com/example/UA/UaApplication.java
+++ src/main/java/com/example/UA/UaApplication.java
@@ -1,4 +1,4 @@
-package com.example.UA.controller;
+package com.example.UA;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
