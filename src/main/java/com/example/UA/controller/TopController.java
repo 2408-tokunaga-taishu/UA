00e1f2b10790424566f5eff388f9a973c93ddc22
murayama_misato <murayama.misato@alhinc.com>
2024-10-09 22:48:00 +0900

    ログインしているアカウントのみの勤怠を表示

--- src/main/java/com/example/UA/controller/TopController.java
+++ src/main/java/com/example/UA/controller/TopController.java
@@ -1,7 +1,9 @@
 package com.example.UA.controller;
 
+import com.example.UA.controller.form.AccountForm;
 import com.example.UA.controller.form.WorkForm;
 import com.example.UA.service.WorkService;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +15,9 @@ import java.util.List;
 public class TopController {
     @Autowired
     WorkService workService;
+
+    @Autowired
+    private HttpSession session;
     /*
      * ホーム画面表示
      */
@@ -20,7 +25,9 @@ public class TopController {
     public ModelAndView top() {
         ModelAndView mav = new ModelAndView();
         List<WorkForm> works = workService.findAllWorks();
+        AccountForm loginAccount = (AccountForm)session.getAttribute("loginAccount");
         mav.addObject("works", works);
+        mav.addObject("loginAccount", loginAccount);
         mav.setViewName("/top");
         return mav;
     }
