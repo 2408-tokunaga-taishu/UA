dbdb4f4ac41fe3b5a799abf390cca04303da26bc
murayama_misato <murayama.misato@alhinc.com>
2024-10-10 11:18:35 +0900

    勤怠表示する月の変更機能実装

--- src/main/java/com/example/UA/controller/TopController.java
+++ src/main/java/com/example/UA/controller/TopController.java
@@ -2,13 +2,16 @@ package com.example.UA.controller;
 
 import com.example.UA.controller.form.AccountForm;
 import com.example.UA.controller.form.WorkForm;
+import com.example.UA.repository.entity.Work;
 import com.example.UA.service.WorkService;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.text.ParseException;
 import java.util.List;
 
 @Controller
@@ -22,14 +25,36 @@ public class TopController {
      * ホーム画面表示
      */
     @GetMapping({"/top", "/"})
-    public ModelAndView top() {
+    public ModelAndView top() throws ParseException {
         ModelAndView mav = new ModelAndView();
         List<WorkForm> works = workService.findAllWorks();
         AccountForm loginAccount = (AccountForm)session.getAttribute("loginAccount");
         mav.addObject("works", works);
+        mav.addObject("displayMonth", workService.getDisplayMonth());
         mav.addObject("loginAccount", loginAccount);
         mav.setViewName("/top");
         return mav;
     }
 
+    /*
+     * 前月のデータ表示(日付絞り込み)
+     */
+    @PostMapping("/previousMonth")
+    public ModelAndView previousMonth() throws ParseException {
+        ModelAndView mav = new ModelAndView();
+        workService.changeMonth(-1);
+        mav.setViewName("redirect:/top");
+        return mav;
+    }
+
+    /*
+     * 次月のデータ表示(日付絞り込み)
+     */
+    @PostMapping("/nextMonth")
+    public ModelAndView nextMonth() {
+        ModelAndView mav = new ModelAndView();
+        workService.changeMonth(1);
+        mav.setViewName("redirect:/top");
+        return mav;
+    }
 }
--- src/main/java/com/example/UA/repository/WorkRepository.java
+++ src/main/java/com/example/UA/repository/WorkRepository.java
@@ -2,8 +2,15 @@ package com.example.UA.repository;
 
 import com.example.UA.repository.entity.Work;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface WorkRepository extends JpaRepository<Work, Integer> {
+    @Query("SELECT w FROM Work w WHERE date BETWEEN :start AND :end ORDER BY date")
+    public List<Work> findByStartAndEnd(@Param("start") Date start, @Param("end") Date end);
 }
